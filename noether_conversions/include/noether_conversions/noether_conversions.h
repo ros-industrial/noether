c3e862adfdf0885cdfcaadd5ea2996c973e8bbfa
AustinDeric <austin.deric@swri.org>
2017-03-03 08:18:56 -0600

    add namespace to noether conversions enforce c++ styleguide (columns) remove extra find_package() for VTK

--- noether_conversions/include/noether_conversions/noether_conversions.h
+++ noether_conversions/include/noether_conversions/noether_conversions.h
@@ -1,5 +1,9 @@
 #include <geometry_msgs/PoseArray.h>
 #include <tool_path_planner/raster_tool_path_planner.h>
+namespace noether {
 
-std::vector<geometry_msgs::PoseArray> posesConvertVTKtoGeometryMsgs(const std::vector<tool_path_planner::ProcessPath>& paths);
+  std::vector<geometry_msgs::PoseArray> convertVTKtoGeometryMsgs(
+      const std::vector<tool_path_planner::ProcessPath>& paths);
+
+}
 
---
+++ noether_conversions/test/utest.cpp
@@ -0,0 +1,13 @@
+#include "noether_conversions/noether_conversions.h"
+#include <gtest/gtest.h>
+
+TEST(TestSuite, GeometryMsgsToVTK_setup)
+{
+  ASSERT_NO_FATAL_FAILURE();
+}
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
