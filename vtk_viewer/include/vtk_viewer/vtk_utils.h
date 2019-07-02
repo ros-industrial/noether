7014222e02890fd2f18092c8ad8d82a618a85a1c
Matthew Powelson <matthew.powelson@swri.org>
2019-07-02 11:11:30 -0500

    Correct License Information to Apache 2.0
    Noether was intended to be released as Apache 2.0 when it was released as open source. However, the license notices were never updated. This commit updates the notices in the headers as well as the package.xml to correctly state the license as Apache 2.0 rather than proprietary.

--- vtk_viewer/include/vtk_viewer/vtk_utils.h
+++ vtk_viewer/include/vtk_viewer/vtk_utils.h
@@ -1,9 +1,21 @@
-/*
- * Copyright (c) 2016, Southwest Research Institute
- * All rights reserved.
+/**
+ * @file vtk_utils.h
+ * @copyright Copyright (c) 2019, Southwest Research Institute
  *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 
 #ifndef NOETHER_VTK_UTILS_H
 #define NOETHER_VTK_UTILS_H
