c10faccd4e2bdc25e7334851455b287ed86ed7b3
Lewis, Colin T <colin.lewis@swri.org>
2019-09-17 10:03:33 -0500

    switching to new branch for refactor

--- mesh_segmenter/include/mesh_segmenter/convex_hull.h
+++ mesh_segmenter/include/mesh_segmenter/convex_hull.h
@@ -12,19 +12,21 @@
 #include <pcl/common/centroid.h>
 #include <Eigen/Dense>
 
+using namespace std; 
+
 class ConvexHullGenerator
 {
 public:
   ConvexHullGenerator(); //constructor
   void makemesh(string input, pcl::PointCloud<pcl::PointXYZ> *inMesh);	
-  bool cleanmesh(pcl::PointCloud<pcl::PointXYZ> outMesh. pcl::PolygonMesh::Ptr outMeshPoly);	
+  bool cleanmesh(pcl::PointCloud<pcl::PointXYZ> outMesh, pcl::PolygonMesh::Ptr outMeshPoly);	
   bool savemesh(pcl::PointCloud<pcl::PointXYZ>::Ptr outMesh, pcl::PolygonMesh::Ptr outMeshPoly, string outfile);
-  bool generate_ch(string Infile, String Outfile);	
+  bool generate_ch(string infile, string outfile);	
 
 private: //What needs to be protected?
 	pcl::PointCloud<pcl::PointXYZ>::Ptr inMesh;
     pcl::PointCloud<pcl::PointXYZ>::Ptr outMesh;
     pcl::PolygonMesh::Ptr outMeshPoly;
     pcl::ConvexHull<pcl::PointXYZ> chull;
-}
+};
 #endif
--- noether/src/convex_hull_node.cpp
+++ noether/src/convex_hull_node.cpp
@@ -2,20 +2,20 @@
 #include <mesh_segmenter/convex_hull.h>
 #include <noether_msgs/GenerateConvexHull.h>	
   
-bool executeCB(const noether_msgs::GenerateConvexHull::Request& req)
+bool executeCB(noether_msgs::GenerateConvexHull::Request& req, noether_msgs::GenerateConvexHull::Response& res)
   {
   	ConvexHullGenerator ch_gen;
 
      //step 1: Read in file
-  	 string inMeshFileName_ = req.file_in; //expects a path to file
-     string modifier_ = "_chullt.ply";
-     outMeshFileName_ = inMeshFileName_.substr(0, inMeshFileName_.size()-4);
-     outMeshFileName_.append(modifier_);
-     
-     //step 2: Call library
-  	 bool success = chgen.generate_ch(inMeshFileName_,outMeshFileName_);
+  	 string inMeshFileName = req.file_in; //expects a path to file
+     string modifier = "_chullt.ply";
+     string outMeshFileName = inMeshFileName.substr(0, inMeshFileName.size()-4);
+     outMeshFileName.append(modifier);
 
-  	 return(success);
+     //step 2: Call library
+  	 bool success = ch_gen.generate_ch(inMeshFileName,outMeshFileName);
+
+  	 return true;
   }
 
 int main(int argc, char **argv)
@@ -23,7 +23,6 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "convex_hull_server");
   ros::NodeHandle nh;
 
-  HullService server(nh);
   ros::ServiceServer service = nh.advertiseService("convex_hull_generation", executeCB);
   ros::spin();
 }
