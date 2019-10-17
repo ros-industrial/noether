4704a6779948310c3b61465ec6b1f66d4babd4d3
mripperger <michael.ripperger@swri.org>
2019-10-17 11:23:59 -0500

    Added pass through filter

---
+++ noether_filtering/include/noether_filtering/cloud/impl/pass_through_filter.hpp
@@ -0,0 +1,80 @@
+#ifndef NOETHER_FILTERING_CLOUD_IMPL_PASS_THROUGH_FILTER_HPP
+#define NOETHER_FILTERING_CLOUD_IMPL_PASS_THROUGH_FILTER_HPP
+
+#include <console_bridge/console.h>
+#include "noether_filtering/cloud/pass_through_filter.h"
+#include "noether_filtering/utils.h"
+#include <pcl/filters/passthrough.h>
+#include <XmlRpcException.h>
+
+namespace noether_filtering
+{
+template<typename PointT>
+bool PassThroughFilter<PointT>::configure(XmlRpc::XmlRpcValue value)
+{
+  // Required parameter(s)
+  try
+  {
+    params.filter_field_name = static_cast<std::string>(value["filter_field_name"]);
+    params.min_limit = static_cast<double>(value["min_limit"]);
+    params.max_limit = static_cast<double>(value["max_limit"]);
+  }
+  catch (const XmlRpc::XmlRpcException &ex)
+  {
+    CONSOLE_BRIDGE_logError("Failed to load required parameter(s) for voxel grid filter: '%s'",
+                            ex.getMessage().c_str());
+    return false;
+  }
+
+  // Optional parameter(s)
+  try
+  {
+    params.negative = static_cast<bool>(value["negative"]);
+  }
+  catch (const XmlRpc::XmlRpcException &ex)
+  {
+    CONSOLE_BRIDGE_logWarn("Failed to load optional parameter(s) for voxel grid filter: '%s'",
+                           ex.getMessage().c_str());
+  }
+
+  return true;
+}
+
+template<typename PointT>
+bool PassThroughFilter<PointT>::filter(const T &input, T &output)
+{
+  output.reset(new pcl::PointCloud<PointT>());
+
+  pcl::PassThrough<PointT> f;
+
+  // Set the parameters
+  if(!params.filter_field_name.empty())
+  {
+    f.setFilterFieldName(params.filter_field_name);
+    f.setFilterLimits(params.min_limit, params.max_limit);
+    f.setFilterLimitsNegative(params.negative);
+  }
+  else
+  {
+    CONSOLE_BRIDGE_logError("No filter field name set for pass through filter");
+    return false;
+  }
+
+  f.setInputCloud(input);
+  f.filter(*output);
+
+  return true;
+}
+
+template<typename PointT>
+std::string PassThroughFilter<PointT>::getName()
+{
+  return utils::getClassName<decltype(this)>();
+}
+
+} // namespace noether_filtering
+
+#define PCL_INSTANTIATE_PassThroughFilter(T) \
+  template class PCL_EXPORTS noether_filtering::PassThroughFilter<T>;
+
+#endif // NOETHER_FILTERING_CLOUD_IMPL_PASS_THROUGH_FILTER_HPP
---
+++ noether_filtering/include/noether_filtering/cloud/pass_through_filter.h
@@ -0,0 +1,33 @@
+#ifndef NOETHER_FILTERING_CLOUD_PASS_THROUGH_FILTER_H
+#define NOETHER_FILTERING_CLOUD_PASS_THROUGH_FILTER_H
+
+#include "noether_filtering/filter_base.h"
+#include <pcl/point_cloud.h>
+
+namespace noether_filtering
+{
+template<typename PointT>
+class PassThroughFilter : public FilterBase<typename pcl::PointCloud<PointT>::Ptr>
+{
+public:
+  using T = typename pcl::PointCloud<PointT>::Ptr;
+
+  using FilterBase<T>::FilterBase;
+
+  virtual bool configure(XmlRpc::XmlRpcValue config) final;
+  virtual bool filter(const T &input, T &output) final;
+  virtual std::string getName() final;
+
+  struct Params
+  {
+    std::string filter_field_name = "x";
+    float min_limit = -std::numeric_limits<float>::max();
+    float max_limit = std::numeric_limits<float>::max();
+    bool negative = false;
+  };
+  Params params;
+};
+
+} // namespace noether_filtering
+
+#endif // NOETHER_FILTERING_CLOUD_PASS_THROUGH_FILTER_H
