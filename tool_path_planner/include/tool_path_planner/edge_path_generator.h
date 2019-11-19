d67ce246d3fae9be3d2e0e9660038334a6291c04
jrgnicho <jrgnichodevel@gmail.com>
2019-11-18 18:46:13 -0600

    splitting edge segments working now, needs cleanup

--- tool_path_planner/include/tool_path_planner/edge_path_generator.h
+++ tool_path_planner/include/tool_path_planner/edge_path_generator.h
@@ -13,6 +13,7 @@
 #include <shape_msgs/Mesh.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/PointIndices.h>
 #include <pcl/octree/octree_search.h>
 
 namespace tool_path_planner
@@ -20,25 +21,43 @@ namespace tool_path_planner
 
 struct EdgePathConfig
 {
-  // compute hsv values
-  double octree_res = 0.005;
-  double search_radius = 0.01;
-  std::size_t num_threads = 4;
+  /**
+   * @ingroup  hsv values computation using the covariance matrix
+   * @{
+   */
+  double octree_res = 0.005;    /** @brief resolution for the octree */
+  double search_radius = 0.01;  /** @brief radious used for grouping */
+  std::size_t num_threads = 4;  /** @brief desired omp thread count  */
+  /** @}*/
 
-  // edge filtering
-  double neighbor_tol = 0.01;
+  /**
+   * @ingroup edge filtering
+   * @{
+   */
+  double neighbor_tol = 0.01;   /** @brief points with "v > max_v * (1 - neighbor_tol)" are considered edge points  */
+  /** @}*/
 
-  // edge segment clustering
-  double cluster_tol = 0.02;
-  int cluster_min = 3;
-  int cluster_max = 1000;
+  /**
+   * @ingroup  edge point reordering
+   * @{
+   */
+  double voxel_size = 0.01;        /** @brief used in downsampling edge points  */
+  int edge_cluster_min = 3;        /** @brief downsamples only when edge cluster size is greater than this value  */
+  double kdtree_epsilon = 0.01;    /** @brief precision used for nearest neighbor searches  */
+  /** @}*/
 
-  // edge point reordering
-  double voxel_size = 0.01;
-  double kdtree_epsilon = 0.01;
+  /**
+   * @ingroup edge segment splitting
+   * @brief uses ray-casting against the mesh's octree minus the edge points.
+   * @{
+   */
+  double min_projection_dist = 0.01;  /** @brief minimum distance by which to project an edge point along the normal,
+                                                 recommended 2 * octree_res */
+  int max_intersecting_voxels = 4;    /** @brief A surface is considered to be between two waypoints when the ray intersects
+                                                 a number of voxels greater than this number*/
+  /** @}*/
 
-  // edge segment splitting
-  int max_intersecting_voxels = 4;
+  double merge_dist = 0.01;
 };
 
 class EdgePathGenerator
@@ -47,24 +66,63 @@ public:
   EdgePathGenerator();
   virtual ~EdgePathGenerator();
 
-  void setInput(pcl::PolygonMesh::ConstPtr mesh, double octree_res = 0.01);
-  void setInput(const shape_msgs::Mesh& mesh, double octree_res = 0.01);
+  /**
+   * @brief sets the input mesh from which edges are to be generated
+   * @param mesh The mesh input
+   */
+  void setInput(pcl::PolygonMesh::ConstPtr mesh);
 
+  /**
+   * @brief sets the input mesh from which edges are to be generated
+   * @param mesh The mesh input
+   */
+  void setInput(const shape_msgs::Mesh& mesh);
+
+  /**
+   * @brief Generate the edge poses that follow the contour of the mesh
+   * @param config The configuration
+   * @return  An array of edge poses or boost::none when it fails.
+   */
   boost::optional< std::vector<geometry_msgs::PoseArray> > generate(const tool_path_planner::EdgePathConfig& config);
+
+  /**
+   * @brief Generate the edge poses that follow the contour of the mesh
+   * @param mesh  The input mesh from which edges will be generated
+   * @param config The configuration
+   * @return  An array of edge poses or boost::none when it fails.
+   */
   boost::optional< std::vector<geometry_msgs::PoseArray> > generate(const shape_msgs::Mesh& mesh,
                                                                     const tool_path_planner::EdgePathConfig& config);
+
+  /**
+   * @brief Generate the edge poses that follow the contour of the mesh
+   * @param mesh  The input mesh from which edges will be generated
+   * @param config The configuration
+   * @return  An array of edge poses or boost::none when it fails.
+   */
   boost::optional< std::vector<geometry_msgs::PoseArray> > generate(pcl::PolygonMesh::ConstPtr mesh,
                                                                     const tool_path_planner::EdgePathConfig& config);
 
 
 protected:
 
-  bool splitEdgeSegments(const tool_path_planner::EdgePathConfig& config, pcl::PointCloud<pcl::PointXYZ>::ConstPtr points ,
-                         const pcl::PointIndices& edge_indices, std::vector<pcl::PointIndices>& segments);
+  /**
+   * @brief splits the edge into multiple segments that are separated by surfaces
+   * @param config        The configuration
+   * @param edge_indices  The indices of the "ordered" points at the edges
+   * @param segments      The output indices of each edge segment
+   * @return  True on success, false otherwise
+   */
+  bool splitEdgeSegments(const tool_path_planner::EdgePathConfig& config, const pcl::PointIndices& edge_indices,
+                         std::vector<pcl::PointIndices>& segments);
+
+
+  int mergePoints(const tool_path_planner::EdgePathConfig& config,const pcl::PointIndices& edge_segment,
+                                      pcl::PointCloud<pcl::PointNormal>& merged_points);
 
   pcl::PointCloud<pcl::PointNormal>::Ptr input_cloud_;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr input_points_;
   pcl::PolygonMesh::ConstPtr mesh_;
-  pcl::PointCloud<pcl::PointXYZHSV>::Ptr edge_results_;
   pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>::Ptr octree_search_;
 
 
