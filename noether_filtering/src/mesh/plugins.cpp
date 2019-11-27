f8f2b6185992c2c9e066ffa71d6e01d67b65fc0d
jrgnicho <jrgnichodevel@gmail.com>
2019-11-26 18:20:47 -0600

    Added configuration for Euclidean Clustering mesh filter

--- noether_filtering/src/mesh/plugins.cpp
+++ noether_filtering/src/mesh/plugins.cpp
@@ -2,5 +2,7 @@
 
 // Mesh filters
 #include "noether_filtering/mesh/bspline_reconstruction.h"
+#include "noether_filtering/mesh/euclidean_clustering.h"
 
 PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::BSplineReconstruction, noether_filtering::mesh::MeshFilterBase)
+PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::EuclideanClustering, noether_filtering::mesh::MeshFilterBase)
