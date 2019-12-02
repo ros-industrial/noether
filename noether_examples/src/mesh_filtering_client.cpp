cc2fd23c4e79b0304451c8d0c9562451924305ec
jrgnicho <jrgnichodevel@gmail.com>
2019-12-02 14:14:30 -0600

    added missing licenses for some files

--- noether_examples/src/mesh_filtering_client.cpp
+++ noether_examples/src/mesh_filtering_client.cpp
@@ -1,9 +1,24 @@
-/*
- * mesh_filtering_client.cpp
+/**
+ * @author Jorge Nicho <jrgnichodevel@gmail.com>
+ * @file mesh_filtering_client.cpp
+ * @date Oct 16, 2019
+ * @copyright Copyright (c) 2019, Southwest Research Institute
  *
- *  Created on: Oct 16, 2019
- *      Author: jrgnicho
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 
 #include <ros/ros.h>
 #include <boost/format.hpp>
