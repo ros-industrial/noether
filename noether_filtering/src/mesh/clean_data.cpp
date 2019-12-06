0bbb63a1ea0cd1f28a9900ce8f555e7e49cdee97
jrgnicho <jrgnichodevel@gmail.com>
2019-12-06 14:07:34 -0600

    added CleanData mesh filter plugin

---
+++ noether_filtering/src/mesh/clean_data.cpp
@@ -0,0 +1,65 @@
+/*
+ * clean_data.cpp
+ *
+ *  Created on: Dec 6, 2019
+ *      Author: jrgnicho
+ */
+
+#include <XmlRpcException.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkCleanPolyData.h>
+#include <pcl/surface/vtk_smoothing/vtk_utils.h>
+#include <console_bridge/console.h>
+#include "noether_filtering/mesh/clean_data.h"
+#include "noether_filtering/utils.h"
+
+namespace noether_filtering
+{
+namespace mesh
+{
+CleanData::CleanData()
+{
+
+}
+
+CleanData::~CleanData()
+{
+
+}
+
+bool CleanData::configure(XmlRpc::XmlRpcValue config)
+{
+  // no configuration needed
+  return true;
+}
+
+bool CleanData::filter(const pcl::PolygonMesh& mesh_in, pcl::PolygonMesh& mesh_out)
+{
+  using namespace pcl;
+
+  vtkSmartPointer<vtkPolyData> mesh_data = vtkSmartPointer<vtkPolyData>::New();
+  VTKUtils::mesh2vtk(mesh_in, mesh_data);
+  std::size_t num_start_points = mesh_data->GetNumberOfPoints();
+
+  mesh_data->BuildCells();
+  mesh_data->BuildLinks();
+
+  vtkSmartPointer<vtkCleanPolyData> cleanPolyData = vtkSmartPointer<vtkCleanPolyData>::New();
+  cleanPolyData->SetInputData(mesh_data);
+  cleanPolyData->Update();
+  mesh_data = cleanPolyData->GetOutput();
+  std::size_t num_end_points = mesh_data->GetNumberOfPoints();
+  CONSOLE_BRIDGE_logInform("Removed duplicate points, retained %lu points from %lu",
+                           num_end_points, num_start_points);
+  VTKUtils::vtk2mesh(mesh_data,mesh_out);
+  return true;
+}
+
+std::string CleanData::getName() const
+{
+  return utils::getClassName<decltype(*this)>();
+}
+
+} /* namespace mesh */
+} /* namespace noether_filtering */
--- noether_filtering/src/mesh/plugins.cpp
+++ noether_filtering/src/mesh/plugins.cpp
@@ -3,6 +3,8 @@
 // Mesh filters
 #include "noether_filtering/mesh/bspline_reconstruction.h"
 #include "noether_filtering/mesh/euclidean_clustering.h"
+#include "noether_filtering/mesh/clean_data.h"
 
 PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::BSplineReconstruction, noether_filtering::mesh::MeshFilterBase)
 PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::EuclideanClustering, noether_filtering::mesh::MeshFilterBase)
+PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::CleanData, noether_filtering::mesh::MeshFilterBase)
