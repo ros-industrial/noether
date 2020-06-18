8e459ebbffdbde082075263a9f25c6229b3d176f
Matthew Powelson <powelson.matthew@gmail.com>
2020-06-18 09:35:56 -0500

    Build BSpline filter only if PCL was built with Nurbs enabled

--- noether_filtering/src/mesh/plugins.cpp
+++ noether_filtering/src/mesh/plugins.cpp
@@ -1,14 +1,17 @@
 #include <pluginlib/class_list_macros.h>
 
 // Mesh filters
-#include "noether_filtering/mesh/bspline_reconstruction.h"
 #include "noether_filtering/mesh/euclidean_clustering.h"
 #include "noether_filtering/mesh/clean_data.h"
 #include "noether_filtering/mesh/windowed_sinc_smoothing.h"
 #include "noether_filtering/mesh/fill_holes.h"
 
-PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::BSplineReconstruction, noether_filtering::mesh::MeshFilterBase)
 PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::EuclideanClustering, noether_filtering::mesh::MeshFilterBase)
 PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::CleanData, noether_filtering::mesh::MeshFilterBase)
 PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::WindowedSincSmoothing, noether_filtering::mesh::MeshFilterBase)
 PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::FillHoles, noether_filtering::mesh::MeshFilterBase)
+
+#ifdef ENABLE_NURBS
+#include "noether_filtering/mesh/bspline_reconstruction.h"
+PLUGINLIB_EXPORT_CLASS(noether_filtering::mesh::BSplineReconstruction, noether_filtering::mesh::MeshFilterBase)
+#endif
