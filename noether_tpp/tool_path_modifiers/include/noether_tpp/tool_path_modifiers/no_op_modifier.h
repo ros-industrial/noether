ea9661b2b9f0eced2e19b38f416849fdbeb3d277
Michael Ripperger <michael.ripperger@swri.org>
2021-07-21 17:38:54 -0500

    Revised tool path modifiers sub-directory

---
+++ noether_tpp/tool_path_modifiers/include/noether_tpp/tool_path_modifiers/no_op_modifier.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <noether_tpp/core/tool_path_modifier.h>
+
+namespace noether_tpp
+{
+/**
+ * @brief A tool path modifier that does not change the input tool path
+ */
+class NoOpToolPathModifier : public OneTimeToolPathModifier
+{
+public:
+  using OneTimeToolPathModifier::OneTimeToolPathModifier;
+
+  inline ToolPaths modify(ToolPaths tool_paths) const override final { return tool_paths; };
+};
+
+} // namespace noether_tpp
