5c7a6f64fd0d7792edfa7749b6279125cc81f481
Michael Ripperger <michael.ripperger@swri.org>
2021-08-25 16:46:36 -0500

    Updated documentation for organization modifiers

--- noether_tpp/include/noether_tpp/tool_path_modifiers/organization_modifiers.h
+++ noether_tpp/include/noether_tpp/tool_path_modifiers/organization_modifiers.h
@@ -6,11 +6,9 @@ namespace noether
 {
 /**
  * @brief Organizes a tool path into a raster-style pattern
- * @details The tool path is organized with the following structure:
- *   - waypoints are sorted within each tool path segment in ascending order by x-axis value
- *   - tool path segments are sorted within each tool path in ascending order by the x-axis value of their first
- * waypoint
- *   - tool paths are sorted in ascending order by the y-axis value of the first waypoint in the first tool path segment
+ * @details  Waypoints are sorted within each tool path segment in ascending order by x-axis value. Tool path segments
+ * are sorted within each tool path in ascending order by the x-axis value of their first waypoint. Tool paths are
+ * sorted in ascending order by the y-axis value of the first waypoint in the first tool path segment.
  */
 struct RasterOrganizationModifier : OneTimeToolPathModifier
 {
@@ -21,13 +19,8 @@ struct RasterOrganizationModifier : OneTimeToolPathModifier
 
 /**
  * @brief Organizes a tool path into a snake-style pattern
- * @details The tool path is organized with the following structure:
- *   - Tool paths at even indices are sorted by ascending waypoint x-axis value; segments in the tool path are also
- * sorted by ascending x-axis value of their first waypoint
- *   - Tool paths at odd indices are sorted by descending waypoint x-axis value; segments in the tool path are also
- * sorted by descending x-axis value of their first waypoint
- *   - Tool paths are sorted relative to one another by ascending y-axis value of the first waypoint in the first tool
- * path segment
+ * @details A RasterOrganizationModifier is first used to organize the tool paths into a raster pattern. The tool paths
+ * at odd indices are then reversed to produce the snake pattern.
  */
 struct SnakeOrganizationModifier : OneTimeToolPathModifier
 {
