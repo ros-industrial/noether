5ff5a832b19cabbbc3d093d2b64312ef6d60dd77
Michael Ripperger <michael.ripperger@swri.org>
2021-08-25 16:48:20 -0500

    Added 'default' modifiers to edge path planner class

--- noether_tpp/src/tool_path_planners/edge_planner.cpp
+++ noether_tpp/src/tool_path_planners/edge_planner.cpp
@@ -2,13 +2,25 @@
 
 #include <utility>  // std::move()
 
+#include <noether_tpp/tool_path_modifiers/organization_modifiers.h>
+#include <noether_tpp/tool_path_modifiers/waypoint_orientation_modifiers.h>
+
 namespace noether
 {
 ToolPaths EdgePlanner::plan(const pcl::PolygonMesh& mesh) const
 {
   ToolPaths tool_paths = planImpl(mesh);
 
-  // To-do: implement the modifications necessary to produce the default behavior
+  // Apply the modifications necessary to produce the "default" behavior
+  // First, organize the position of the waypoints into standard edge paths
+  StandardEdgePathsOrganizationModifier edge_path_organizer;
+  tool_paths = edge_path_organizer.modify(tool_paths);
+
+  // Next, update the orientation of the waypoints such that their x-axes align with the direction of travel between
+  // adjacent waypoints. Note: this modifier does not change the z-axis of the waypoints (normal to the surface)
+  // returned by the planner implementation
+  DirectionOfTravelOrientationModifier dot_orientation;
+  tool_paths = dot_orientation.modify(tool_paths);
 
   return tool_paths;
 }
