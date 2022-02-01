4f0a139cbf4cd57e2d26046f5927d28278a5ef4d
Tyler Marr <tylermarr17@gmail.com>
2022-02-01 10:34:44 -0600

    Fixed extra raster_directions param that slipped through on rebase

--- tool_path_planner/include/tool_path_planner/plane_slicer_raster_generator.h
+++ tool_path_planner/include/tool_path_planner/plane_slicer_raster_generator.h
@@ -63,7 +63,6 @@ public:
     bool interleave_rasters{ DEFAULT_INTERLEAVE_RASTERS };
     bool smooth_rasters{ DEFAULT_SMOOTH_RASTERS };
     bool raster_wrt_global_axes{ DEFAULT_RASTER_WRT_GLOBAL_AXES };
-    Eigen::Vector3d raster_direction{ Eigen::Vector3d::Zero() };
     bool generate_extra_rasters{ DEFAULT_GENERATE_EXTRA_RASTERS };
     Eigen::Vector3d raster_direction{ Eigen::Vector3d::UnitY() };
     RasterStyle raster_style{ DEFAULT_RASTER_STYLE };
