4f156df6211f0628c5fe940e3c13c73055b8f65e
Michael Ripperger <michael.ripperger@swri.org>
2022-06-17 18:12:23 -0500

    Replace tool_path_planner version of plane slice raster planner with noether_tpp version

--- tool_path_planner/include/tool_path_planner/plane_slicer_raster_generator.h
+++ tool_path_planner/include/tool_path_planner/plane_slicer_raster_generator.h
@@ -22,22 +22,16 @@
 #ifndef INCLUDE_PLANE_SLICER_RASTER_GENERATOR_H_
 #define INCLUDE_PLANE_SLICER_RASTER_GENERATOR_H_
 
-#include <vtkPoints.h>
-#include <vtkSmartPointer.h>
-#include <vtkPolyData.h>
-#include <boost/optional.hpp>
-#include <pcl/PolygonMesh.h>
-#include <shape_msgs/Mesh.h>
-#include <vtkCellLocator.h>
-#include <vtkKdTreePointLocator.h>
-#include <jsoncpp/json/json.h>
-#include <ros/console.h>
-
 #include <tool_path_planner/path_generator.h>
+
+#include <boost/optional.hpp>
+#include <jsoncpp/json/json.h>
+#include <pcl/PolygonMesh.h>
 
 namespace tool_path_planner
 {
-class PlaneSlicerRasterGenerator : public PathGenerator
+class [[deprecated("Replaced by PlaneSliceRasterPlanner in noether_tpp")]] PlaneSlicerRasterGenerator
+  : public PathGenerator
 {
   static constexpr double DEFAULT_RASTER_SPACING = 0.04;
   static constexpr double DEFAULT_POINT_SPACING = 0.01;
@@ -56,73 +50,10 @@ public:
     double search_radius{ DEFAULT_SEARCH_RADIUS };
     double min_hole_size{ DEFAULT_MIN_HOLE_SIZE };
 
-    Json::Value toJson() const
-    {
-      Json::Value jv(Json::ValueType::objectValue);
-      jv["raster_spacing"] = raster_spacing;
-      jv["point_spacing"] = point_spacing;
-      jv["raster_rot_offset"] = raster_rot_offset;
-      jv["min_segment_size"] = min_segment_size;
-      jv["search_radius"] = search_radius;
-      jv["min_hole_size"] = min_hole_size;
-
-      return jv;
-    }
-
-    bool fromJson(const Json::Value& jv)
-    {
-      if (jv.isNull())
-      {
-        ROS_ERROR("Json value is null");
-        return false;
-      }
-      if (jv.type() != Json::ValueType::objectValue)
-      {
-        ROS_ERROR("Json type %i is invalid, only '%i' is allowed",
-                  static_cast<int>(jv.type()),
-                  static_cast<int>(Json::ValueType::objectValue));
-        return false;
-      }
-      auto validate = [](const Json::Value& jv, const std::string& name_, const Json::ValueType& type_) -> bool {
-        return jv.isMember(name_) && jv[name_].type() == type_;
-      };
-      raster_spacing = validate(jv, "raster_spacing", Json::ValueType::realValue) ? jv["raster_spacing"].asDouble() :
-                                                                                    DEFAULT_RASTER_SPACING;
-      point_spacing = validate(jv, "point_spacing", Json::ValueType::realValue) ? jv["point_spacing"].asDouble() :
-                                                                                  DEFAULT_POINT_SPACING;
-      raster_rot_offset = validate(jv, "raster_rot_offset", Json::ValueType::realValue) ?
-                              jv["raster_rot_offset"].asDouble() :
-                              DEFAULT_RASTER_ROT_OFFSET;
-      min_segment_size = validate(jv, "min_segment_size", Json::ValueType::realValue) ?
-                             jv["min_segment_size"].asDouble() :
-                             DEFAULT_MIN_SEGMENT_SIZE;
-      search_radius = validate(jv, "search_radius", Json::ValueType::realValue) ? jv["search_radius"].asDouble() :
-                                                                                  DEFAULT_SEARCH_RADIUS;
-      min_hole_size = validate(jv, "min_hole_size", Json::ValueType::realValue) ? jv["min_hole_size"].asDouble() :
-                                                                                  DEFAULT_MIN_HOLE_SIZE;
-      return true;
-    }
-
-    bool fromJson(const std::string& jv_string)
-    {
-      Json::Value jv;
-      Json::Reader r;
-      if (!r.parse(jv_string, jv))
-        return false;
-
-      return fromJson(jv);
-    }
-
-    std::string str()
-    {
-      std::stringstream ss;
-      ss << "raster_spacing: " << raster_spacing << std::endl;
-      ss << "point_spacing: " << point_spacing << std::endl;
-      ss << "raster_rot_offset: " << raster_rot_offset << std::endl;
-      ss << "min_segment_size: " << min_segment_size << std::endl;
-      ss << "search_radius: " << search_radius << std::endl;
-      return ss.str();
-    }
+    Json::Value toJson() const;
+    bool fromJson(const Json::Value& jv);
+    bool fromJson(const std::string& jv_string);
+    std::string str();
   };
 
   PlaneSlicerRasterGenerator() = default;
@@ -147,11 +78,7 @@ public:
   std::string getName() const override;
 
 private:
-  bool insertNormals(const double search_radius, vtkSmartPointer<vtkPolyData>& data);
-
-  vtkSmartPointer<vtkPolyData> mesh_data_;
-  vtkSmartPointer<vtkKdTreePointLocator> kd_tree_;
-  vtkSmartPointer<vtkCellLocator> cell_locator_;
+  pcl::PolygonMesh mesh_data_;
   Config config_;
 };
 
