3770131cbf7d6f52a4e1bffcbb78ee26e3581d1f
Michael Ripperger <michael.ripperger@swri.org>
2022-07-01 18:33:20 -0500

    Convert from assert to exceptions

--- noether_gui/include/noether_gui/utils.h
+++ noether_gui/include/noether_gui/utils.h
@@ -29,13 +29,18 @@ inline QStringList getAvailablePlugins(const boost_plugin_loader::PluginLoader& 
  */
 inline void overwriteWidget(QLayout* layout, QWidget*& from, QWidget* to)
 {
-  assert(from != nullptr);
-  assert(to != nullptr);
-  assert(layout != nullptr);
+  if (!from)
+    throw std::runtime_error("From widget is a nullptr");
+  if (!to)
+    throw std::runtime_error("To widget is a nullptr");
+  if (!layout)
+    throw std::runtime_error("Layout is a nullptr");
 
   QLayoutItem* ret = layout->replaceWidget(from, to);
-  assert(ret != nullptr);
-  assert(ret->widget() == from);
+  if (!ret)
+    throw std::runtime_error("Widget replacement returned a nullptr");
+  if (ret->widget() == from)
+    throw std::runtime_error("The widget returned by the replacement did not change");
 
   // Delete the replaced widget (which is no longer managed), and reset it to the widget that replaced it for future
   // reference
