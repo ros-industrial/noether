0f587dd129facfb9e82d7fc730193d22f5667e99
[david spielman] <[david.spielman@swri.org]>
2023-05-04 17:20:37 -0500

    Created utils.h and utils.cpp to hold useful function definitions

--- noether_tpp/src/tool_path_modifiers/organization_modifiers.cpp
+++ noether_tpp/src/tool_path_modifiers/organization_modifiers.cpp
@@ -1,58 +1,10 @@
 #include <noether_tpp/tool_path_modifiers/organization_modifiers.h>
+#include <noether_tpp/utils.h>
 
 #include <numeric>
 
 namespace noether
 {
-/** @brief Estimates the direction of travel of a tool path by averaging the vectors between adjacent waypoints */
-static Eigen::Vector3d estimateToolPathDirection(const ToolPath& tool_path)
-{
-  Eigen::Vector3d avg_dir = Eigen::Vector3d::Zero();
-  int n = 0;
-  for (const ToolPathSegment& seg : tool_path)
-  {
-    if (seg.size() > 1)
-    {
-      for (std::size_t i = 0; i < seg.size() - 1; ++i)
-      {
-        avg_dir += (seg.at(i + 1).translation() - seg.at(i).translation()).normalized();
-        ++n;
-      }
-    }
-  }
-
-  if (n < 1)
-    throw std::runtime_error("Insufficient number of points to calculate average tool path direction");
-
-  avg_dir /= static_cast<double>(n);
-
-  return avg_dir.normalized();
-}
-
-/**
- * @brief Estimates the raster direction of a set of tool paths by returning the component of the vector from the first
- * point in the first segment of the first tool path) to the first waypoint in the first segment of the last tool path
- * that is perpendicular to the nominal tool path direction
- */
-static Eigen::Vector3d estimateRasterDirection(const ToolPaths& tool_paths,
-                                               const Eigen::Vector3d& reference_tool_path_dir)
-{
-  // First waypoint in the first segment of the first tool path
-  const Eigen::Isometry3d first_wp = tool_paths.at(0).at(0).at(0);
-  // First waypoint in the first segment of the last tool path
-  const Eigen::Isometry3d first_wp_last_path = tool_paths.at(tool_paths.size() - 1).at(0).at(0);
-
-  // Normalize the reference tool path direction
-  const Eigen::Vector3d norm_ref_tool_path_dir = reference_tool_path_dir.normalized();
-
-  // Get the vector between the two points
-  Eigen::Vector3d raster_dir = first_wp_last_path.translation() - first_wp.translation();
-
-  // Subtract the component of this vector that is parallel to the reference tool path direction
-  raster_dir -= raster_dir.dot(norm_ref_tool_path_dir) * norm_ref_tool_path_dir;
-  return raster_dir.normalized();
-}
-
 ToolPaths RasterOrganizationModifier::modify(ToolPaths tool_paths) const
 {
   ToolPathSegment first_seg = tool_paths.at(0).at(0);
