44bdb70be4061c28173251d8f932d67f552277dd
[david spielman] <[david.spielman@swri.org]>
2023-05-05 14:15:20 -0500

    Remove duplicated comments

--- noether_tpp/src/utils.cpp
+++ noether_tpp/src/utils.cpp
@@ -2,7 +2,6 @@
 
 namespace noether
 {
-/** @brief Estimates the direction of travel of a tool path by averaging the vectors between adjacent waypoints */
 Eigen::Vector3d estimateToolPathDirection(const ToolPath& tool_path)
 {
   Eigen::Vector3d avg_dir = Eigen::Vector3d::Zero();
@@ -27,11 +26,6 @@ Eigen::Vector3d estimateToolPathDirection(const ToolPath& tool_path)
   return avg_dir.normalized();
 }
 
-/**
- * @brief Estimates the raster direction of a set of tool paths by returning the component of the vector from the first
- * point in the first segment of the first tool path) to the first waypoint in the first segment of the last tool path
- * that is perpendicular to the nominal tool path direction
- */
 Eigen::Vector3d estimateRasterDirection(const ToolPaths& tool_paths, const Eigen::Vector3d& reference_tool_path_dir)
 {
   // First waypoint in the first segment of the first tool path
