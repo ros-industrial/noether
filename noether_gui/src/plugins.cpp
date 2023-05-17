7ec7101783d9159da83a38e1ba6d535b42171c99
Michael Ripperger <michael.ripperger@swri.org>
2023-05-17 18:25:06 -0500

    Added compile definitions for plugin loader environment variables (#186)

--- noether_gui/src/plugins.cpp
+++ noether_gui/src/plugins.cpp
@@ -76,6 +76,9 @@ struct PlaneSlicerRasterPlannerWidgetPlugin : ToolPathPlannerWidgetPlugin
   {
     boost_plugin_loader::PluginLoader loader;
     loader.search_libraries.insert(NOETHER_GUI_PLUGINS);
+    loader.search_libraries_env = NOETHER_GUI_PLUGIN_LIBS_ENV;
+    loader.search_paths_env = NOETHER_GUI_PLUGIN_PATHS_ENV;
+
     auto widget = new PlaneSlicerRasterPlannerWidget(std::move(loader), parent);
 
     // Attempt to configure the widget
