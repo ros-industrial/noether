fe0ade705e33beb6baa07a4e6d2b42097bc8e05b
David Spielman <davidroyspielman@gmail.com>
2023-09-06 17:39:08 -0500

    Created toolpath modifier that creates a height offset from the first point on a raster and creates a desired number of points between the offset and the point on the part. The gui currently crashes when the plugin is added

--- noether_gui/src/plugins.cpp
+++ noether_gui/src/plugins.cpp
@@ -22,6 +22,7 @@
 #include <noether_gui/widgets/tool_path_modifiers/standard_edge_paths_organization_modifier_widget.h>
 #include <noether_gui/widgets/tool_path_modifiers/tool_drag_orientation_modifier_widget.h>
 #include <noether_gui/widgets/tool_path_modifiers/uniform_orientation_modifier_widget.h>
+#include <noether_gui/widgets/tool_path_modifiers/straight_approach_modifier_widget.h>
 
 #include <QWidget>
 #include <QMessageBox>
@@ -87,6 +88,9 @@ using CircularLeadInToolPathModifierWidgetPlugin =
 using CircularLeadOutToolPathModifierWidgetPlugin =
     WidgetPluginImpl<CircularLeadOutToolPathModifierWidget, ToolPathModifierWidget>;
 
+using StraightApproachToolPathModifierWidgetPlugin =
+    WidgetPluginImpl<StraightApproachToolPathModifierWidget, ToolPathModifierWidget>;
+
 // Raster Tool Path Planners
 struct PlaneSlicerRasterPlannerWidgetPlugin : ToolPathPlannerWidgetPlugin
 {
@@ -131,5 +135,6 @@ EXPORT_TOOL_PATH_MODIFIER_WIDGET_PLUGIN(noether::ToolDragOrientationToolPathModi
                                         ToolDragOrientationToolPathModifier)
 EXPORT_TOOL_PATH_MODIFIER_WIDGET_PLUGIN(noether::CircularLeadInToolPathModifierWidgetPlugin, CircularLeadInModifier)
 EXPORT_TOOL_PATH_MODIFIER_WIDGET_PLUGIN(noether::CircularLeadOutToolPathModifierWidgetPlugin, CircularLeadOutModifier)
+EXPORT_TOOL_PATH_MODIFIER_WIDGET_PLUGIN(noether::StraightApproachToolPathModifierWidgetPlugin, StraightApproachModifier)
 
 EXPORT_TPP_WIDGET_PLUGIN(noether::PlaneSlicerRasterPlannerWidgetPlugin, PlaneSlicerRasterPlanner)
