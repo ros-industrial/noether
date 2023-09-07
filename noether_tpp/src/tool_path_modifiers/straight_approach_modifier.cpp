647ef8b7d5ae67f5fd603ff3738cbae90b15993d
David Spielman <davidroyspielman@gmail.com>
2023-09-07 15:01:26 -0500

    Removed typo from noether_tpp/src/tool_path_modifiers/straight_approach_modifier.cpp in call to header file. Removed some comments from noether_gui/src/widgets/tool_path_modifiers/straight_approach_modifier_widget.cpp

--- noether_tpp/src/tool_path_modifiers/straight_approach_modifier.cpp
+++ noether_tpp/src/tool_path_modifiers/straight_approach_modifier.cpp
@@ -1,4 +1,4 @@
-#include <noether_tpp/tool_path_modifiers/straight_approach_modifier.h>.h>
+#include <noether_tpp/tool_path_modifiers/straight_approach_modifier.h>
 #include <noether_tpp/utils.h>
 
 namespace noether
@@ -10,16 +10,12 @@ StraightApproachModifier::StraightApproachModifier(double offset_height, double 
 
 ToolPaths StraightApproachModifier::modify(ToolPaths tool_paths) const
 {
-//  double delta_theta = arc_angle_ / (n_points_ - 1);
   for (ToolPath& tool_path : tool_paths)
   {
     Eigen::Vector3d dir = estimateToolPathDirection(tool_path);
     const Eigen::Isometry3d& first = tool_path.at(0).at(0);
     const Eigen::Vector3d& y = first.matrix().col(1).head<3>();
     const Eigen::Vector3d& z = first.matrix().col(2).head<3>();
-    // Sign is determined by whether the first point's y-axis is aligned with the nominal path y-axis (i.e., the cross
-    // of the waypoint's z-axis with the nominal path direction)
-    double sign = z.cross(dir).dot(y) > 0.0 ? 1.0 : -1.0;
     for (ToolPathSegment& segment : tool_path)
     {
       Eigen::Isometry3d radius_center = segment.front() * Eigen::Translation3d(0.0, 0.0, offset_height_);
@@ -28,11 +24,7 @@ ToolPaths StraightApproachModifier::modify(ToolPaths tool_paths) const
       for (int i = 0; i < n_points_; i++)
       {
         Eigen::Isometry3d pt;
-//        if (i == 0 || i==n_points_-1){
-          /*Eigen::Isometry3d */pt = radius_center *
-//                               Eigen::AngleAxisd(sign * delta_theta * (i + 1), Eigen::Vector3d::UnitY()) *
-                               Eigen::Translation3d(0, 0, -offset_height_ + (offset_height_/(n_points_))*i);
-//        }
+        pt = radius_center * Eigen::Translation3d(0, 0, -offset_height_ + (offset_height_/(n_points_))*i);
         pt.linear() = segment.front().linear();
         new_segment.push_back(pt);
       }
