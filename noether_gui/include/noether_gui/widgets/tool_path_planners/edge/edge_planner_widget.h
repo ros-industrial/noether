10b7cf33d1885405612cb43dc4266485611a494b
Michael Ripperger <michael.ripperger@swri.org>
2024-09-27 16:15:56 -0500

    Added definition of edge tool path planner widget

---
+++ noether_gui/include/noether_gui/widgets/tool_path_planners/edge/edge_planner_widget.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <noether_gui/widgets.h>
+#include <noether_tpp/core/tool_path_planner.h>
+
+namespace noether
+{
+using EdgePlannerWidget = ToolPathPlannerWidget;
+
+}  // namespace noether
