959a3b60511e9b439ae1e89eff6e3ce012b09a9a
Michael Ripperger <michael.ripperger@swri.org>
2024-09-27 15:36:28 -0500

    Added implementation of half-edge edge planner

---
+++ noether_tpp/include/noether_tpp/tool_path_planners/edge/half_edge_planner.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <noether_tpp/tool_path_planners/edge/edge_planner.h>
+
+namespace noether
+{
+/**
+ * @brief Edge tool path planner that creates paths around the boundary edges of a mesh (exterior and interior) as
+ * identified by half-edge triangle mesh representation of the mesh
+ */
+class HalfEdgePlanner : public EdgePlanner
+{
+public:
+  HalfEdgePlanner();
+
+  ToolPaths planImpl(const pcl::PolygonMesh&) const override;
+};
+
+}  // namespace noether
