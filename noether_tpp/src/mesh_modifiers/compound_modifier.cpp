ros-industrial/noether

commit 614ff5df6995356fe7c886f2ba0940f13590d38b

Fix error reporting around missing vertex normals (#283)

noether_tpp/src/mesh_modifiers/compound_modifier.cpp
@@ -7,19 +7,31 @@ CompoundMeshModifier::CompoundMeshModifier(std::vector<MeshModifier::ConstPtr> m
 {
 }
 
-std::vector<pcl::PolygonMesh> CompoundMeshModifier::modify(const pcl::PolygonMesh& mesh) const
+std::vector<pcl::PolygonMesh> CompoundMeshModifier::modify(const pcl::PolygonMesh& original_mesh) const
 {
   // Create a vector of meshes as the input and output to each modifier
   // Seed it with the single input mesh for the first modifier
-  std::vector<pcl::PolygonMesh> meshes{ mesh };
+  std::vector<pcl::PolygonMesh> meshes{ original_mesh };
 
-  for (const MeshModifier::ConstPtr& modifier : modifiers_)
+  for (std::size_t i = 0; i < modifiers_.size(); ++i)
   {
+    const MeshModifier::ConstPtr& modifier = modifiers_[i];
+
     std::vector<pcl::PolygonMesh> tmp_meshes;
-    for (const pcl::PolygonMesh& mesh : meshes)
+    for (std::size_t j = 0; j < meshes.size(); ++i)
     {
-      auto tmp = modifier->modify(mesh);
-      tmp_meshes.insert(tmp_meshes.end(), tmp.begin(), tmp.end());
+      try
+      {
+        const pcl::PolygonMesh& mesh = meshes[j];
+        auto tmp = modifier->modify(mesh);
+        tmp_meshes.insert(tmp_meshes.end(), tmp.begin(), tmp.end());
+      }
+      catch (const std::exception& ex)
+      {
+        std::stringstream ss;
+        ss << "Error invoking mesh modifier at index " << i << " on submesh at index " << j << ".";
+        std::throw_with_nested(std::runtime_error(ss.str()));
+      }
     }
 
     // Overwrite the meshes for the next modifier

noether_tpp/src/tool_path_modifiers/compound_modifier.cpp
@@ -11,9 +11,19 @@ CompoundModifier::CompoundModifier(std::vector<ToolPathModifier::ConstPtr> modif
 ToolPaths CompoundModifier::modify(ToolPaths paths) const
 {
   // Apply the modifiers in order
-  for (const auto& modifier : modifiers_)
+  for (std::size_t i = 0; i < modifiers_.size(); ++i)
   {
-    paths = modifier->modify(paths);
+    try
+    {
+      const auto& modifier = modifiers_[i];
+      paths = modifier->modify(paths);
+    }
+    catch (const std::exception& ex)
+    {
+      std::stringstream ss;
+      ss << "Error applying tool path modifier at index " << i << ".";
+      std::throw_with_nested(std::runtime_error(ss.str()));
+    }
   }
   return paths;
 }
@@ -26,9 +36,19 @@ OneTimeCompoundModifier::OneTimeCompoundModifier(std::vector<OneTimeToolPathModi
 ToolPaths OneTimeCompoundModifier::modify(ToolPaths paths) const
 {
   // Apply the modifiers in order
-  for (const auto& modifier : modifiers_)
+  for (std::size_t i = 0; i < modifiers_.size(); ++i)
   {
-    paths = modifier->modify(paths);
+    try
+    {
+      const auto& modifier = modifiers_[i];
+      paths = modifier->modify(paths);
+    }
+    catch (const std::exception& ex)
+    {
+      std::stringstream ss;
+      ss << "Error applying tool path modifier at index " << i << ".";
+      std::throw_with_nested(std::runtime_error(ss.str()));
+    }
   }
   return paths;
 }

.github/workflows/clang_format.yml
@@ -15,14 +15,14 @@ on:
 
 jobs:
   clang_format:
-    runs-on: ubuntu-20.04
+    runs-on: ubuntu-22.04
     steps:
       - uses: actions/checkout@v4
 
       - name: Run clang format
         run: |
           sudo apt update
-          sudo apt install -y git clang-format
+          sudo apt install -y git clang-format-14
           if [ $? -ge 1 ]; then return 1; fi
           ./.run-clang-format
           if [ $? -ge 1 ]; then return 1; fi

.run-clang-format
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\|hxx\)' -exec clang-format -style=file -i {} \;
+find . -type f -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\|hxx\)' -exec clang-format-14 -style=file -i {} \;

noether_gui/src/widgets/tpp_widget.cpp
@@ -400,20 +400,50 @@ void TPPWidget::onPlan(const bool /*checked*/)
     QApplication::setOverrideCursor(Qt::WaitCursor);
 
     // Run the mesh modifier
-    std::vector<pcl::PolygonMesh> meshes = pipeline.mesh_modifier->modify(full_mesh);
+    std::vector<pcl::PolygonMesh> meshes;
+    try
+    {
+      meshes = pipeline.mesh_modifier->modify(full_mesh);
+    }
+    catch (const std::exception& ex)
+    {
+      std::throw_with_nested(std::runtime_error("Error invoking mesh modifier"));
+    }
 
     tool_paths_.clear();
     tool_paths_.reserve(meshes.size());
     std::vector<ToolPaths> unmodified_tool_paths;
     unmodified_tool_paths.reserve(meshes.size());
 
-    for (const pcl::PolygonMesh& mesh : meshes)
+    for (std::size_t i = 0; i < meshes.size(); ++i)
     {
+      const pcl::PolygonMesh& mesh = meshes[i];
+
       // Plan the tool path
-      ToolPaths path = pipeline.planner->plan(mesh);
+      ToolPaths path;
+      try
+      {
+        path = pipeline.planner->plan(mesh);
+      }
+      catch (const std::exception& ex)
+      {
+        std::stringstream ss;
+        ss << "Error invoking tool path planner on mesh at index " << i << " in the TPP pipeline.";
+        std::throw_with_nested(std::runtime_error(ss.str()));
+      }
 
       unmodified_tool_paths.push_back(path);
-      tool_paths_.push_back(pipeline.tool_path_modifier->modify(path));
+
+      try
+      {
+        tool_paths_.push_back(pipeline.tool_path_modifier->modify(path));
+      }
+      catch (const std::exception& ex)
+      {
+        std::stringstream ss;
+        ss << "Error invoking tool path modifier for tool path at index " << i << " in the TPP pipeline.";
+        std::throw_with_nested(std::runtime_error(ss.str()));
+      }
     }
 
     QApplication::restoreOverrideCursor();

noether_tpp/src/core/tool_path_planner_pipeline.cpp
@@ -11,16 +11,46 @@ ToolPathPlannerPipeline::ToolPathPlannerPipeline(MeshModifier::ConstPtr mesh_mod
 {
 }
 
-std::vector<ToolPaths> ToolPathPlannerPipeline::plan(pcl::PolygonMesh mesh) const
+std::vector<ToolPaths> ToolPathPlannerPipeline::plan(pcl::PolygonMesh original_mesh) const
 {
-  std::vector<pcl::PolygonMesh> meshes = mesh_modifier->modify(mesh);
+  std::vector<pcl::PolygonMesh> meshes;
+  try
+  {
+    meshes = mesh_modifier->modify(original_mesh);
+  }
+  catch (const std::exception& ex)
+  {
+    std::throw_with_nested(std::runtime_error("Error invoking mesh modifier in TPP pipeline."));
+  }
 
   std::vector<ToolPaths> output;
   output.reserve(meshes.size());
-  for (const pcl::PolygonMesh& mesh : meshes)
+  for (std::size_t i = 0; i < meshes.size(); ++i)
   {
-    ToolPaths path = planner->plan(mesh);
-    output.push_back(tool_path_modifier->modify(path));
+    const pcl::PolygonMesh& mesh = meshes[i];
+
+    ToolPaths path;
+    try
+    {
+      path = planner->plan(mesh);
+    }
+    catch (const std::exception& ex)
+    {
+      std::stringstream ss;
+      ss << "Error invoking tool path planner on mesh at index " << i << " in TPP pipeline.";
+      std::throw_with_nested(std::runtime_error(ss.str()));
+    }
+
+    try
+    {
+      output.push_back(tool_path_modifier->modify(path));
+    }
+    catch (const std::exception& ex)
+    {
+      std::stringstream ss;
+      ss << "Error invoking tool path modifier on the tool path at index " << i << " in the TPP pipeline";
+      std::throw_with_nested(std::runtime_error(ss.str()));
+    }
   }
 
   return output;

noether_tpp/src/tool_path_planners/raster/plane_slicer_raster_planner.cpp
@@ -449,7 +449,14 @@ void PlaneSlicerRasterPlanner::generateRastersBidirectionally(const bool bidirec
 ToolPaths PlaneSlicerRasterPlanner::planImpl(const pcl::PolygonMesh& mesh) const
 {
   if (!hasNormals(mesh))
-    throw std::runtime_error("Mesh does not have vertex normals");
+  {
+    std::stringstream ss;
+    ss << "The input mesh does not have vertex normals, which are required for the plane slice raster tool path "
+          "planner. "
+       << "Use a MeshModifier to generate vertex normals for the mesh, or provide a different mesh with vertex "
+          "normals.";
+    throw std::runtime_error(ss.str());
+  }
 
   // Convert input mesh to VTK type & calculate normals if necessary
   vtkSmartPointer<vtkPolyData> mesh_data_ = vtkSmartPointer<vtkPolyData>::New();

noether_tpp/src/utils.cpp
@@ -92,7 +92,7 @@ Eigen::Vector3f getNormal(const pcl::PCLPointCloud2& cloud, const std::uint32_t 
   auto nz_it = noether::findField(cloud.fields, "normal_z");
 
   if (nx_it == cloud.fields.end() || ny_it == cloud.fields.end() || nz_it == cloud.fields.end())
-    throw std::runtime_error("Not all normals exist");
+    throw std::runtime_error("Not all vertex normal fields exist in the point cloud");
 
   const std::uint32_t offset = pt_idx * cloud.point_step;
   const auto* nx = reinterpret_cast<const float*>(cloud.data.data() + offset + nx_it->offset);

commit ef32744f07cb3abacc86e06c91e351357c3f1dbf

Fixed bug in compound modifier introduced in #283

noether_tpp/src/mesh_modifiers/compound_modifier.cpp
@@ -18,7 +18,7 @@ std::vector<pcl::PolygonMesh> CompoundMeshModifier::modify(const pcl::PolygonMes
     const MeshModifier::ConstPtr& modifier = modifiers_[i];
 
     std::vector<pcl::PolygonMesh> tmp_meshes;
-    for (std::size_t j = 0; j < meshes.size(); ++i)
+    for (std::size_t j = 0; j < meshes.size(); ++j)
     {
       try
       {