f144ef4dcc43fccd67a7e986b26518920d9165bb
Michael Ripperger <michael.ripperger@swri.org>
2025-07-15 14:12:49 -0500

    Added plugins for mesh modifier implementations

--- noether_tpp/src/plugins.cpp
+++ noether_tpp/src/plugins.cpp
@@ -1,6 +1,15 @@
 #include <noether_tpp/plugin_interface.h>
 #include <memory>
 
+// Mesh modifiers
+#include <noether_tpp/mesh_modifiers/clean_data_modifier.h>
+#include <noether_tpp/mesh_modifiers/euclidean_clustering_modifier.h>
+#include <noether_tpp/mesh_modifiers/fill_holes_modifier.h>
+#include <noether_tpp/mesh_modifiers/normal_estimation_pcl.h>
+#include <noether_tpp/mesh_modifiers/normals_from_mesh_faces_modifier.h>
+#include <noether_tpp/mesh_modifiers/plane_projection_modifier.h>
+#include <noether_tpp/mesh_modifiers/windowed_sinc_smoothing_modifier.h>
+
 namespace noether
 {
 template <typename DerivedT, typename BaseT>
@@ -12,4 +21,21 @@ struct PluginImpl : public Plugin<BaseT>
   }
 };
 
+// Mesh Modifiers
+using CleanDataMeshModifierPlugin = PluginImpl<CleanData, MeshModifier>;
+using EuclideanClusteringMeshModifierPlugin = PluginImpl<EuclideanClusteringMeshModifier, MeshModifier>;
+using FillHolesMeshModifierPlugin = PluginImpl<FillHoles, MeshModifier>;
+using NormalEstimationPCLMeshModifierPlugin = PluginImpl<NormalEstimationPCLMeshModifier, MeshModifier>;
+using NormalsFromMeshFacesMeshModifierPlugin = PluginImpl<NormalsFromMeshFacesMeshModifier, MeshModifier>;
+using PlaneProjectionMeshModifierPlugin = PluginImpl<PlaneProjectionMeshModifier, MeshModifier>;
+using WindowedSincSmoothingMeshModifierPlugin = PluginImpl<WindowedSincSmoothing, MeshModifier>;
+
 }  // namespace noether
+
+EXPORT_MESH_MODIFIER_PLUGIN(noether::CleanDataMeshModifierPlugin, CleanData)
+EXPORT_MESH_MODIFIER_PLUGIN(noether::EuclideanClusteringMeshModifierPlugin, EuclideanClustering)
+EXPORT_MESH_MODIFIER_PLUGIN(noether::FillHolesMeshModifierPlugin, FillHoles)
+EXPORT_MESH_MODIFIER_PLUGIN(noether::NormalEstimationPCLMeshModifierPlugin, NormalEstimationPCL)
+EXPORT_MESH_MODIFIER_PLUGIN(noether::NormalsFromMeshFacesMeshModifierPlugin, NormalsFromMeshFaces)
+EXPORT_MESH_MODIFIER_PLUGIN(noether::PlaneProjectionMeshModifierPlugin, PlaneProjection)
+EXPORT_MESH_MODIFIER_PLUGIN(noether::WindowedSincSmoothingMeshModifierPlugin, WindowedSincSmoothing)
