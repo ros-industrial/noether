649cbe1b1058181c7d16a638ff7adf41564fa0f6
Michael Ripperger <michael.ripperger@swri.org>
2025-12-31 14:17:32 -0600

    Specified front and back offsets separately

--- noether_gui/src/widgets/tool_path_modifiers/spline_extrapolation_modifier_widget.cpp
+++ noether_gui/src/widgets/tool_path_modifiers/spline_extrapolation_modifier_widget.cpp
@@ -3,11 +3,11 @@
 
 #include <noether_tpp/serialization.h>
 
-static const char* EXTRAPOLATION_DISTANCE_KEY = "extrapolation_distance";
-static const char* NORMAL_OFFSET_DISTANCE_KEY = "normal_offset_distance";
-static const char* EXTRAPOLATE_FRONT_KEY = "extrapolate_front";
-static const char* EXTRAPOLATE_BACK_KEY = "extrapolate_back";
 static const char* SPLINE_DEGREE_KEY = "spline_degree";
+static const char* EXTRAPOLATION_DISTANCE_FRONT_KEY = "extrapolation_distance_front";
+static const char* NORMAL_OFFSET_DISTANCE_FRONT_KEY = "normal_offset_distance_front";
+static const char* EXTRAPOLATION_DISTANCE_BACK_KEY = "extrapolation_distance_back";
+static const char* NORMAL_OFFSET_DISTANCE_BACK_KEY = "normal_offset_distance_back";
 
 namespace noether
 {
@@ -15,25 +15,75 @@ SplineExtrapolationToolPathModifierWidget::SplineExtrapolationToolPathModifierWi
   : BaseWidget(parent), ui_(new Ui::SplineExtrapolation())
 {
   ui_->setupUi(this);
+
+  auto configure = [this](int state) {
+    switch (state)
+    {
+      case Qt::CheckState::Checked:
+        // Disable the back spin boxes
+        ui_->extrapolation_distance_back->setEnabled(false);
+        ui_->normal_offset_distance_back->setEnabled(false);
+
+        // Connect the front extrapolation distance spin box to the back
+        connect(ui_->extrapolation_distance_front,
+                qOverload<double>(&QDoubleSpinBox::valueChanged),
+                ui_->extrapolation_distance_back,
+                &QDoubleSpinBox::setValue);
+
+        // Connect the front normal offset distance spin box to the back
+        connect(ui_->normal_offset_distance_front,
+                qOverload<double>(&QDoubleSpinBox::valueChanged),
+                ui_->normal_offset_distance_back,
+                &QDoubleSpinBox::setValue);
+
+        emit ui_->extrapolation_distance_front->valueChanged(ui_->extrapolation_distance_front->value());
+        emit ui_->normal_offset_distance_front->valueChanged(ui_->normal_offset_distance_front->value());
+
+        break;
+      default:
+        // Enable the back spin boxes
+        ui_->extrapolation_distance_back->setEnabled(true);
+        ui_->normal_offset_distance_back->setEnabled(true);
+
+        // Disconnect the front extrapolation distance spin box from the back
+        disconnect(ui_->extrapolation_distance_front,
+                   qOverload<double>(&QDoubleSpinBox::valueChanged),
+                   ui_->extrapolation_distance_back,
+                   &QDoubleSpinBox::setValue);
+
+        // Disonnect the front normal offset distance spin box from the back
+        disconnect(ui_->normal_offset_distance_front,
+                   qOverload<double>(&QDoubleSpinBox::valueChanged),
+                   ui_->normal_offset_distance_back,
+                   &QDoubleSpinBox::setValue);
+
+        break;
+    }
+  };
+
+  connect(ui_->lock_values, &QCheckBox::stateChanged, configure);
+
+  // Call
+  configure(Qt::CheckState::Checked);
 }
 
 void SplineExtrapolationToolPathModifierWidget::configure(const YAML::Node& config)
 {
-  ui_->double_spin_box_extrapolation_distance->setValue(YAML::getMember<double>(config, EXTRAPOLATION_DISTANCE_KEY));
-  ui_->double_spin_box_normal_offset_distance->setValue(YAML::getMember<double>(config, NORMAL_OFFSET_DISTANCE_KEY));
-  ui_->check_box_extrapolate_front->setChecked(YAML::getMember<bool>(config, EXTRAPOLATE_FRONT_KEY));
-  ui_->check_box_extrapolate_back->setChecked(YAML::getMember<bool>(config, EXTRAPOLATE_BACK_KEY));
-  ui_->spin_box_spline_degree->setValue(YAML::getMember<double>(config, SPLINE_DEGREE_KEY));
+  ui_->spline_degree->setValue(YAML::getMember<double>(config, SPLINE_DEGREE_KEY));
+  ui_->extrapolation_distance_front->setValue(YAML::getMember<double>(config, EXTRAPOLATION_DISTANCE_FRONT_KEY));
+  ui_->normal_offset_distance_front->setValue(YAML::getMember<double>(config, NORMAL_OFFSET_DISTANCE_FRONT_KEY));
+  ui_->extrapolation_distance_back->setValue(YAML::getMember<double>(config, EXTRAPOLATION_DISTANCE_BACK_KEY));
+  ui_->normal_offset_distance_back->setValue(YAML::getMember<double>(config, NORMAL_OFFSET_DISTANCE_BACK_KEY));
 }
 
 void SplineExtrapolationToolPathModifierWidget::save(YAML::Node& config) const
 {
   config["name"] = "SplineExtrapolation";
-  config[EXTRAPOLATION_DISTANCE_KEY] = ui_->double_spin_box_extrapolation_distance->value();
-  config[NORMAL_OFFSET_DISTANCE_KEY] = ui_->double_spin_box_normal_offset_distance->value();
-  config[EXTRAPOLATE_FRONT_KEY] = ui_->check_box_extrapolate_front->isChecked();
-  config[EXTRAPOLATE_BACK_KEY] = ui_->check_box_extrapolate_back->isChecked();
-  config[SPLINE_DEGREE_KEY] = ui_->spin_box_spline_degree->value();
+  config[SPLINE_DEGREE_KEY] = ui_->spline_degree->value();
+  config[EXTRAPOLATION_DISTANCE_FRONT_KEY] = ui_->extrapolation_distance_front->value();
+  config[NORMAL_OFFSET_DISTANCE_FRONT_KEY] = ui_->normal_offset_distance_front->value();
+  config[EXTRAPOLATION_DISTANCE_BACK_KEY] = ui_->extrapolation_distance_back->value();
+  config[NORMAL_OFFSET_DISTANCE_BACK_KEY] = ui_->normal_offset_distance_back->value();
 }
 
 }  // namespace noether
--- noether_tpp/include/noether_tpp/tool_path_modifiers/spline_extrapolation_modifier.h
+++ noether_tpp/include/noether_tpp/tool_path_modifiers/spline_extrapolation_modifier.h
@@ -12,8 +12,8 @@ namespace noether
  * @brief Extrapolates the tool path
  * @details This modifier creates additional extrapolation waypoints at the front and/or back of a ToolPathSegment,
  * following the approximate curvature of the tool path (opposed to a linear offset). It works by fitting a spline to
- * the waypoints of each ToolPathSegment and extrapolating beyond the end of the spline using the last polynomial segment.
- * The extrapolated waypoint(s) can also include an optional offset along the waypoint normal (i.e., z-axis).
+ * the waypoints of each ToolPathSegment and extrapolating beyond the end of the spline using the last polynomial
+ * segment. The extrapolated waypoint(s) can also include an optional offset along the waypoint normal (i.e., z-axis).
  */
 class SplineExtrapolationToolPathModifier : public ToolPathModifier
 {
@@ -21,16 +21,18 @@ public:
   /**
    * @brief Constructor
    * @param spline_degree Degree of the spline to fit to the tool path (e.g., 2 (parabolic), 3 (cubic))
-   * @param extrapolation_distance Distance (m) beyond te to extrapolate the tool path
-   * @param normal_offset_distance Offset distance (m) to apply along the extrapolated waypoint(s) normal (i.e., z-axis)
-   * @param extrapolate_front Flag to indicate if the tool path should be extrapolated in the front (i.e., start)
-   * @param extrapolate_back Flag to indicate if the tool path should be extrapolated in the back (i.e., end)
+   * @param extrapolation_distance_front Distance (m) in front of the spline to extrapolate the tool path
+   * @param normal_offset_distance_front Offset distance (m) to apply along the front extrapolated waypoint(s) normal
+   * (i.e., z-axis)
+   * @param extrapolation_distance_back Distance (m) beyond the end of the spline to extrapolate the tool path
+   * @param normal_offset_distance_back Offset distance (m) to apply along the back extrapolated waypoint(s) normal
+   * (i.e., z-axis)
    */
   SplineExtrapolationToolPathModifier(const double spline_degree,
-                                      const double extrapolation_distance,
-                                      const double normal_offset_distance,
-                                      const bool extrapolate_front,
-                                      const bool extrapolate_back);
+                                      const double extrapolation_distance_front,
+                                      const double normal_offset_distance_front,
+                                      const double extrapolation_distance_back,
+                                      const double normal_offset_distance_back);
 
   ToolPaths modify(ToolPaths tool_path) const override;
 
@@ -39,10 +41,10 @@ protected:
   DECLARE_YAML_FRIEND_CLASSES(SplineExtrapolationToolPathModifier);
 
   double spline_degree_;
-  double extrapolation_distance_;
-  double normal_offset_distance_;
-  bool extrapolate_front_;
-  bool extrapolate_back_;
+  double extrapolation_distance_front_;
+  double normal_offset_distance_front_;
+  double extrapolation_distance_back_;
+  double normal_offset_distance_back_;
 };
 
 }  // namespace noether
